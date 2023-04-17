186d2c1ff1679d783a565485d8f27ad0c379bce6
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-17 17:33:35 +0200

    add exercise email

--- script.js
+++ script.js
@@ -23,11 +23,18 @@ const mail = prompt('inserisci la tua mail')
     'mastrogeppetto@libero.it',
     'filippoalbertone@gmail.com'
  ] 
-
+ let hoTrovatoMail = 0
 
  for(let i = 0; i < ListaMail.length;i++){
-    console.log(ListaMail[i])
-    
+   if(mail == ListaMail[i]){
+    hoTrovatoMail = 1
+   }
+ }
+
+ if (hoTrovatoMail ==0){
+    console.log('email non autorizzata');
+ } else {
+    console.log('email autorizzata');
  }
 
 
